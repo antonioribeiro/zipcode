80e9692e590546399c0d1fc9c590b9a62e99092a
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-04-25 08:50:37 -0300

    Add web services for France

--- src/Support/WebServices/Countries/FR.php
+++ src/Support/WebServices/Countries/FR.php
@@ -9,33 +9,17 @@ return [
 		[
 			'name' => 'zippopotam',
 			'url' => 'http://api.zippopotam.us',
-			'query' => '/US/%s',
+			'query' => '/FR/%s',
 			'zip_format' => '99999',
 			'fields' => [
 				'zip' => 'post code',
 				'state_id' => 'places.0.state abbreviation',
 				'state_name' => 'places.0.state',
-				'city' => 'places.0.place name',
+				'neighborhood' => 'places.0.place name',
 				'country_id' => 'country abbreviation',
 				'country_name' => 'country',
 				'longitude' => 'places.0.longitude',
 				'latitude' => 'places.0.latitude',
-			],
-		],
-
-		[
-		 'name' => 'elevenbasetwo',
-			'url' => 'http://zip.elevenbasetwo.com',
-			'query' => '/v2/US/%s',
-			'zip_format' => '99999',
-			'fields' => [
-				'zip' => 'zip',
-				'state_id' => null,
-				'state_name' => 'state',
-				'city' => 'city',
-				'country_id' => 'country',
-				'street_kind' => null,
-				'street_name' => null,
 			],
 		],
 
