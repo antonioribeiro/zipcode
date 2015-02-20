be95681849f7e285cbf30187d62f424d3f04ae04
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-02-20 12:02:16 -0200

    Remove license from files

--- src/Vendor/Laravel/Facade.php
+++ src/Vendor/Laravel/Facade.php
@@ -1,22 +1,6 @@
-<?php namespace PragmaRX\ZipCode\Vendor\Laravel;
-/**
- * Part of the ZipCode package.
- *
- * NOTICE OF LICENSE
- *
- * Licensed under the 3-clause BSD License.
- *
- * This source file is subject to the 3-clause BSD License that is
- * bundled with this package in the LICENSE file.  It is also available at
- * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
- *
- * @package    ZipCode
- * @version    0.1.0
- * @author     Antonio Carlos Ribeiro @ PragmaRX
- * @license    BSD License (3-clause)
- * @copyright  (c) 2013, PragmaRX
- * @link       http://pragmarx.com
- */
+<?php
+
+namespace PragmaRX\ZipCode\Vendor\Laravel;
 
 use Illuminate\Support\Facades\Facade as LaravelFacade;
 
